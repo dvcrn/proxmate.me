2ff6284ddc2b220aedf58902ef616a8c650a10d4
David Mohl <me@dave.cx>
2014-05-05 17:49:23 +0900

    fixed unit test

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function(config) {
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-touch/angular-touch.js',
       'app/bower_components/angular-carousel/dist/angular-carousel.js',
-      'app/bower_components/angular-utils/src/angularUtils.js',
+      'app/bower_components/angular-utils/src/directives/disqus/dirDisqus.js',
       'app/scripts/*.coffee',
       'app/scripts/**/*.coffee',
       'test/mock/**/*.coffee',
